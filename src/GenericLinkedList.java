2233dfa3d7bcd57fc4a3de2fa96cb6ea2e656ff3
michael <michaelnorman4421@gmail.com>
2021-03-21 23:44:11 +0000

    implemented add() method to GenericLinkedList

--- src/GenericLinkedList.java
+++ src/GenericLinkedList.java
@@ -1,11 +1,26 @@
 public class GenericLinkedList<T> {
         Node head; // head of list
+        Node tail; // tail of list
+        int size; //size of list
 
-        public void addToStart(int val)
+        public void add(T val){
+            if (size>0) {
+                Node prev = tail;
+                tail = new Node(val);
+                prev.next = tail;
+            }
+            else {
+                tail = new Node(val);
+                head = tail;
+            }
+            size++;
+        }
+        public void addToStart(T val)
         {
             Node newNode = new Node(val);
             newNode.next = head;
             head = newNode;
+            size++;
         }
 
         public void printList()
@@ -18,15 +33,22 @@ public class GenericLinkedList<T> {
             }
         }
 
-        /* Linked list Node*/
-        private class Node {
-            int data;
-            Node next;
+    public int size() {
+        return size;
+    }
 
+    public T get(int i) {
+            return null;
+    }
+
+    /* Linked list Node*/
+        private class Node{
+            T data;
+            Node next;
             // Constructor to create a new node
-            Node(int d) {
-                data = d;
-                next = null;
+            Node(T d) {
+               this.data = d;
+               this.next = null;
             }
         }
     }
--- src/SimpleGenericsTester.java
+++ src/SimpleGenericsTester.java
@@ -1,32 +1,57 @@
+import java.util.LinkedList;
+
 public class SimpleGenericsTester {
     public static void main(String[] args) throws Exception {
+
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("Brian");
+        names.add("June");
+
         GenericArrayList<Integer> aList = new GenericArrayList<>();
         aList.add(5); aList.add(3); aList.add(8);
         printArrayList("aList", aList);
+
+
         GenericArrayList<String> aSecondList = new GenericArrayList<>();
         aSecondList.add("Tom"); aSecondList.add("Dick"); aSecondList.add("Harry");
         printArrayList("aSecondList", aSecondList);
+
         //Now - a similar test for the generic linked list class
         GenericLinkedList<String> aThirdList = new GenericLinkedList<>();
-        aThirdList.add("Me"); aThirdList.add("Myself"); aThirdList.add("I");
+        aThirdList.addToStart("First");
+        aThirdList.addToStart("Second");
+        aThirdList.addToStart("Third");
+
+        GenericLinkedList<String> aTestList = new GenericLinkedList<>();
+        aTestList.add("First add");
+        aTestList.add("Second add");
+        aTestList.add("Third add");
+
+
+//        aThirdList.addToStart("Me");
+//        aThirdList.addToStart("Myself");
+//        aThirdList.addToStart("I");
+
+        System.out.println("aThirdList size:" + aThirdList.size);
+        System.out.println("aTestList size:" + aTestList.size);
         printLinkedList("aThirdList", aThirdList);
+        printLinkedList("aTestList", aTestList);
     }
-    private static <T> void printArrayList(String listName, GenericArrayList<T>
-            listToPrint) {
+    private static <T> void printArrayList(String listName, GenericArrayList<T> listToPrint) {
         System.out.println(listName + " contains:");
         for(int i = 0; i < listToPrint.size(); i++)
         {
             System.out.print(listToPrint.get(i) + " ");
         }
-        System.out.println();
+        System.out.println("\n");
     }
-    private static <T> void printLinkedList(String listName, GenericLinkedList<T>
-            listToPrint) throws Exception {
+    private static <T> void printLinkedList(String listName, GenericLinkedList<T> listToPrint) throws Exception {
         System.out.println(listName + " contains:");
-        for(int i = 0; i < listToPrint.size(); i++)
-        {
-            System.out.print(listToPrint.get(i) + " ");
-        }
+        listToPrint.printList();
+//        for(int i = 0; i < listToPrint.size(); i++)
+//        {
+//            System.out.print(listToPrint.get(i) + " ");
+//        }
         System.out.println();
     }
 
